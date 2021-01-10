a4d2da81ae8d947e75ddf3642e393207a7cf8239
Ricardo Maldonado <rimaldonado@servexternos.santandertecnologia.com.ar>
2021-01-10 04:50:03 -0300

    update AuthReducer

--- src/auth/authReducer.js
+++ src/auth/authReducer.js
@@ -1,22 +1,21 @@
 import { types } from "../types/types";
 
+export const authReducer = (state = {}, action = null) => {
+  if (!action) return state;
 
+  switch (action.type) {
+    case types.login:
+      return {
+        ...action.payload,
+        logged: true,
+      };
 
-export const authReducer = (state ={}, action) => {
+    case types.logout:
+      return {
+        logged: false,
+      };
 
-    switch (action.type) {
-        case types.login:
-                return{
-                    ...action.payload,
-                    logged: true
-                }
-    
-        case types.logout:
-            return {
-                logged: false
-            }
-
-        default:
-            return state;
-    }    
-}
+    default:
+      return state;
+  }
+};
